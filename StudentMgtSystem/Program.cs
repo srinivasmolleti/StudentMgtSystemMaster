d319a518e9756bc256dced58e6330950f1e79eb8
srini <srinivasmolleti@gmail.com>
2016-09-17 15:17:02 +0800

    tp 1101 Initial commit without any code changes.

---
+++ StudentMgtSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMgtSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
