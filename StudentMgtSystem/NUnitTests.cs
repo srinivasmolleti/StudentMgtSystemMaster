800cf109ad20a814c2c9de2e774bd2c2a6ad591a
srini <srinivasmolleti@gmail.com>
2016-09-18 19:51:27 +0800

    tp 1105 Unit Test Framework Incorporated.

---
+++ StudentMgtSystem/NUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+    
+
+namespace TailSpinToys.UnitTesting
+{
+
+    [TestFixture]
+    public class NUnitTests
+    {
+        [Test]
+        public void SumOfTwoNumbers()
+        {
+            Assert.AreEqual(10, 5 + 5);
+        }
+
+        [Test]
+        public void AreTheValuesTheSame()
+        {
+            Assert.AreSame(10, 5 + 6);
+        }
+    }
+}
